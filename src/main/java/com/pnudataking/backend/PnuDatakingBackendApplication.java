61c8303baae098b9ffbfb7859f32766ef6956503
pykido <animoto1@naver.com>
2025-09-28 18:45:06 +0900

    initial spring boot project setup

---
+++ src/main/java/com/pnudataking/backend/PnuDatakingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pnudataking.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnuDatakingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnuDatakingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnudataking/backend/PnuDatakingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnudataking.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnuDatakingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
